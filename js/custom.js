e7371570ac2922c7879bba74028c5f8f92c8adcb
Agrim Asthana <dev@agrimasthana.com>
2018-12-08 18:03:00 -0600

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function() {
+	"use strict";
+	
+	// PreLoader
+	$(window).load(function() {
+		$(".loader").fadeOut(400);
+	});
+
+	// Backstretchs
+	$("#header").backstretch("images/3.jpg");
+    
+});
